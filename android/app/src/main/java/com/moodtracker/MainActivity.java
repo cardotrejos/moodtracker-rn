0c881339c0426e8d43ccc77eed08a939e6b373e5
Ricardo Trejos <cardotrejos@gmail.com>
2021-11-14 20:50:09 -0500

    App completed

--- android/app/src/main/java/com/moodtracker/MainActivity.java
+++ android/app/src/main/java/com/moodtracker/MainActivity.java
@@ -1,6 +1,12 @@
 package com.moodtracker;
 
 import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+import android.os.Bundle;
+import org.devio.rn.splashscreen.SplashScreen;
+import android.os.Bundle;
 
 public class MainActivity extends ReactActivity {
 
@@ -12,4 +18,21 @@ public class MainActivity extends ReactActivity {
   protected String getMainComponentName() {
     return "MoodTracker";
   }
+
+  @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new ReactActivityDelegate(this, getMainComponentName()) {
+      @Override
+      protected ReactRootView createRootView() {
+       return new RNGestureHandlerEnabledRootView(MainActivity.this);
+      }
+    };
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+   SplashScreen.show(this);
+   super.onCreate(savedInstanceState);
+}
+
 }
